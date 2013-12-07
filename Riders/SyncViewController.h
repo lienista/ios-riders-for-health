f8103865d3d7a2457b46fb7014aaa1eff9269d97
Lien Nguyen <lienista@liens-mbp.corp.onekingslane.biz>
2013-12-06 19:29:16 -0800

    Added Sync View Controller

---
+++ Riders/SyncViewController.h
@@ -0,0 +1,13 @@
+//
+//  SyncViewController.h
+//  Riders
+//
+//  Created by Lienne Nguyen on 12/6/13.
+//  Copyright (c) 2013 Lienne Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SyncViewController : UITableViewController
+
+@end
